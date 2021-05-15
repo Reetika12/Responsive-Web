78e687949e7cdeb63c850a14fc51f9c562a2adf9
Reetika <reetika@Reetikas-MacBook-Pro.local>
2021-05-15 13:45:42 +0530

    first commit for responsive web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <nav className="navbar background">
+          <ul className="nav-list">
+             <div className="logo">
+               <img
+                 src={require("./Image/logo.svg").default}
+                 alt="logo"
+               />
+               <li><a href="#home">Home</a></li>
+               <li><a href="#about">About</a></li>
+               <li><a href="#services">Services</a></li>
+               <li><a href="#contact">Contact Us</a></li>
+             </div>
+          </ul>
+          <div className="rightNav">
+            <input type="text" name="search" id="search"/>
+            <button className="btn btn-sm">Search</button>
+          </div>
+      </nav>
+      <section className="background firstsection">
+        <div className="box-main">
+            <div className="firsthalf">
+                
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
