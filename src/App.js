1aedd22041a0d78f42a63e8b089af3d7f2271194
Reetika <reetika@Reetikas-MacBook-Pro.local>
2021-05-16 16:11:02 +0530

    added contact us form

--- src/App.js
+++ src/App.js
@@ -39,9 +39,58 @@ function App() {
             </div>
         </div>
       </section>
-      <section className="secRight">
-        <p className="sectionTag"></p>
-        <p className="sectionSubTag"></p>
+      <section className="section">
+        <div className="paras">
+          <p className="sectionTag text-big">The end of search is here.</p>
+          <p className="sectionSubTag text-small">Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password lorem password Lorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem
+          Lorem password Lorem password Lorem password.the end of seracch is here. The end of search is here. the end of search is here  the end of search is here. the end of serch is here.
+          password. tHe end of search is here the end of search is here</p>
+        </div>
+         <div className="thumbnail">
+            <img
+                src={require("./Image/sideImage.jpeg").default}
+                alt="laptop image"
+                />
+        </div>
+      </section>
+      <section className="section section-left">
+        <div className="paras">
+          <p className="sectionTag text-big">Transforming Education in India</p>
+          <p className="sectionSubTag text-small">Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password lorem password Lorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem
+          Lorem password Lorem password Lorem password.the end of seracch is here. The end of search is here. the end of search is here  the end of search is here. the end of serch is here.
+          password. tHe end of search is here the end of search is here</p>
+        </div>
+         <div className="thumbnail">
+            <img
+                src={require("./Image/book1.jpeg").default}
+                alt="laptop image"
+                />
+        </div>
+      </section>
+      <section className="section section-left">
+        <div className="paras">
+          <p className="sectionTag text-big">The Change is in Our Hand</p>
+          <p className="sectionSubTag text-small">Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password Lorem password lorem password Lorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem passwordLorem
+          Lorem password Lorem password Lorem password.the end of seracch is here. The end of search is here. the end of search is here  the end of search is here. the end of serch is here.
+          password. tHe end of search is here the end of search is here</p>
+        </div>
+         <div className="thumbnail">
+            <img
+                src={require("./Image/book2.jpeg").default}
+                alt="laptop image"
+                />
+        </div>
+      </section>
+      <hr />
+      <section className="contact">
+          <h2 className="text-align">Contact Us</h2>
+          <div className="form">
+              <input className="form-input" type="text" name="name" id="name" placeholder="Enter Your name"></input>
+              <input className="form-input" type="text" name="phone" id="phone" placeholder="Enter Your phone"></input>
+              <input className="form-input" type="email" name="email" id="email" placeholder="Enter Your email"></input>
+              <textarea className="form-input" name="text" id="text" cols="30" rows="10"placeholder="Ellaborate your concern"></textarea>
+              <button className="btn btn-dark">Submit</button>
+          </div>
       </section>
     </div>
   );
