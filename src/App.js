089fc38e31d1329867c62546ccea37386bfafd93
Reetika12 <reetika.singh05@gmail.com>
2021-06-10 11:32:10 +0530

    added accordian collapsable component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import ResponsiveWeb from './Container/ResponsiveWeb'
 import UseOfStyledComponent from './Container/UseOfStyledComponent'
+import Accordian from '../src/Components/Accordian'
 function App() {
 
   return (
     <div >
-      <ResponsiveWeb/>
+      {/* <ResponsiveWeb/> */}
+      <Accordian/>
       {/* <UseOfStyledComponent/> */}
     </div>
   );
---
+++ src/Components/Data.js
@@ -0,0 +1,19 @@
+export const Data=[
+    {
+        question:'What do you call a dog magician?',
+        answer:'A labracoderjedhed'
+    },
+    {
+        question:'What do you call a funny mountain?',
+        answer:'Hill arious'
+    },
+    {
+        question:'What did the astranaut saywhen he crashed into the moon?',
+        answer:'I apolozise guys'
+    },
+    {
+        question:'What do you call a dog magician?',
+        answer:'A labracoderjedhed'
+    }
+]
+// export default Data;
